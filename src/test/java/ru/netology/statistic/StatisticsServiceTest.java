297b18e8b76fa8fd4376e01d74454fc20c21727e
Мария <airbelka@bk.ru>
2022-05-28 23:20:00 +0300

    Reformat code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,7 +19,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaximum(){
+    void findMaximum() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 20, 12};
